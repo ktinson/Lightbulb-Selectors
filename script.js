8370d2e73094a62832bb0cd322fa267a0e618c41
ktinson <monikai.tinson@verizon.com>
2024-09-04 08:15:23 -0500

    W3D4 Light branch

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Write your code here
+console.log("hello")
+// May need to remove the hashes
+const light1 = document.querySelector("#lightbulb1")
+const light2 = document.querySelector("#lightbulb2")
+const light3 = document.querySelector("#lightbulb3")
+let subTitle = document.querySelector(".subtitle")
+let count = 0
+light1.addEventListener("click", function(){
+    count++
+    subTitle.innerHTML=`You've clicked the lights ${count} times`
+    console.log(subTitle.innerHTML=`You've clicked the lights ${count} times`)
+})
+light2.addEventListener("click", function(){
+    count++
+    subTitle.innerHTML=`You've clicked the lights ${count} times`
+    console.log(subTitle.innerHTML=`You've clicked the lights ${count} times`)
+
+})
+light3.addEventListener("click", function(){
+    count++
+    subTitle.innerHTML=`You've clicked the lights ${count} times`
+    console.log(subTitle.innerHTML=`You've clicked the lights ${count} times`)
+
+})
+console.log(light1)
+console.log(light2)
+console.log(light3)
+console.log(subTitle)
+console.log(count)
